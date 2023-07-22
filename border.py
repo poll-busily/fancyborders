39a57b883b21dd2fc7adc2d102f864dc69f3ddb0
Paul Beesley <830976+poll-busily@users.noreply.github.com>
2023-07-22 20:55:43 +0100

    Use TOML-formatted config files

--- border.py
+++ border.py
@@ -1,90 +1,98 @@
-from __future__ import print_function
-from wand.image import Image
-from wand.color import Color
-from wand.font import Font
-from wand.drawing import Drawing
+# Copyright 2023, Paul Beesley
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from pathlib import Path
+from wand.color import Color
+from wand.drawing import Drawing
+from wand.image import Image
 
+import anyconfig
 import argparse
-import sys
+import logging
 import math
-import os
 
-border_bottom_pct = 0.025
-#border_color = Color('#F7F6F3')
 border_color = Color('#FFFFFF')
 signature_color = Color('#898989')
-# handwriting = Font('handwriting.ttf', color=signature_color)
 
 parser = argparse.ArgumentParser(description='Add title and copyright to image for print')
 parser.add_argument('path', type=str, help='Input file path (not modified in place)')
 parser.add_argument('title', type=str, help='Image title (centered)')
 parser.add_argument('year', type=str, default='',help='Year of shot')
-parser.add_argument('--web', action='store_true', required=False, help='Create a smaller, lower quality product image')
+
+def load_config():
+    conf_default = anyconfig.load("profiles/default.toml")
+
+    logging.warning('Using default configuration as none was specified')
+    return conf_default
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    
     args = parser.parse_args()
+    conf = load_config()
 
     file_path = Path(args.path)
+    logging.info(f'Input image:{file_path}')
     filename = file_path.name
-    filename_no_ext = filename.replace(file_path.suffix, '')
-    output_filename = file_path.with_name(filename_no_ext + '_b.jpg')
-    output_filename_web = file_path.with_name(filename_no_ext + '_b_sml.jpg')
+    filename_with_suffix = filename.replace(file_path.suffix, f'{conf["output"]["suffix"]}')
+    output_path = file_path.with_name(filename_with_suffix + '.jpg')
+    logging.info(f'Output image:{output_path}')
 
     with Image(filename=file_path) as input_image:
         input_w = input_image.width
         input_h = input_image.height
-        print(f"Input image: {filename} @ {input_w}x{input_h}")
+        logging.info(f"Input resolution: {input_w}x{input_h}")
 
-        border_bottom_px = math.trunc((input_h + input_w) * border_bottom_pct)
-        border_other_px = math.trunc(border_bottom_px * 0.55)
-        print(f"Borders: {border_bottom_px}px at bottom, {border_other_px}px around")
+        border_bottom_px = math.trunc((input_h + input_w) * conf['border']['bottom_scale'])
+        border_other_px = math.trunc((input_h + input_w) * conf['border']['top_side_scale'])
+        logging.debug(f"Borders: {border_bottom_px}px at bottom, {border_other_px}px around")
 
         with input_image.clone() as output_image:
             output_image.border(border_color, int(border_other_px), int(border_bottom_px))
-            output_image.crop(left=0, top = math.trunc(border_bottom_px * 0.6))
-
-            # output_image.caption("Paul Beesley Photography",
-            #                      gravity='south',
-            #                      left=0,
-            #                      top=math.trunc(output_image.height * 0.94),
-            #                      font=handwriting,
-            #                      width = math.trunc(input_image.width),
-            #                      height= math.trunc(border_bottom_px * 0.65))
+            output_image.crop(left=0, top = math.trunc(border_bottom_px * 0.5)) # The top border will be the same as the bottom otherwise
 
             with Drawing() as ctx:
-                ctx.font = 'DejaVu Sans Oblique'
-                ctx.font_family = 'DejaVu Sans'
-                ctx.font_size = (input_w + input_h) * 0.006
-                ctx.fill_color = signature_color
+                ctx.font = conf['font']['name']
+                ctx.font_family = conf['font']['family']
+                ctx.font_size = (input_w + input_h) * conf['font']['scale_factor_title']
+                ctx.fill_color = Color(conf['font']['color'])
                 ctx.gravity = "south"
-                ctx.font_weight = 10
+                ctx.font_weight = conf['font']['weight']
                 output_image.annotate(args.title, ctx, 0, math.trunc(border_bottom_px * 0.39))
 
             with Drawing() as ctx2:
-                ctx2.font = 'DejaVu Sans Oblique'
-                ctx2.font_family = 'DejaVu Sans'
-                ctx2.font_size = (input_w + input_h) * 0.0045
-                ctx2.fill_color = signature_color
+                ctx2.font = conf['font']['name']
+                ctx2.font_family = conf['font']['family']
+                ctx2.font_size = (input_w + input_h) * conf['font']['scale_factor_metadata']
+                ctx2.fill_color = Color(conf['font']['color'])
                 ctx2.gravity = "south_west"
-                ctx2.font_weight = 10
-                output_image.annotate("Paul Beesley Landscape Photography", ctx2, border_other_px, math.trunc(border_bottom_px * 0.55))
+                ctx2.font_weight = conf['font']['weight']
+                output_image.annotate(conf['author']['name'], ctx2, border_other_px, math.trunc(border_bottom_px * 0.55))
                 output_image.annotate("© " + args.year, ctx2, border_other_px, math.trunc(border_bottom_px * 0.31))
 
-            output_image.compression_quality = 99
-            output_image.format = 'jpeg'
-            output_image.save(filename=output_filename)
-            print(f"Saved full-size output to: {output_filename}")
-            
-            if (args.web):
-                megapixels = 3
-                pixels = megapixels * 1000000
-                print(f"Resizing to {megapixels} megapixels ")
+            if (conf['resize']['enable']):
+                logging.info(f"Resizing to {conf['resize']['megapixels']} megapixels ")
+                pixels = conf['resize']['megapixels'] * 1000000
+                input_pixels = input_w * input_h
+                if input_pixels < pixels:
+                    logging.warning('Input image is smaller than output - you are scaling up!')
                 output_image.transform('',f'{pixels}@')
-                output_image.compression_quality = 65
-                output_image.save(filename=output_filename_web)
-                print(f"Saved small output to: {output_filename_web}")
-                
+
+            match conf['output']['codec']:
+                case 'jpeg':
+                    output_image.compression_quality = int(conf['output']['quality'])
+                    output_image.format = 'jpeg'
+                case 'png':
+                    output_image.format = 'png'
+                case 'tiff':
+                    output_image.format = 'tiff'
+                case _:
+                    logging.warning("Unrecognised output format: using PNG")
+                    output_image.format = 'png'
+            
+            
+            output_image.save(filename=output_path)
+            logging.info("All done!")
 
 if __name__ == "__main__":
     main()
